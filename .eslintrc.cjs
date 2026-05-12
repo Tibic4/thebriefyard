6a3d93010707f895cab55df7f8fdfc8b60303112
Tibic4 <bicagold@gmail.com>
2026-05-12 20:18:48 -0300

    chore: add prettier root config and @briefyard/eslint-config package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   root: true,
   extends: ['@briefyard/eslint-config'],
-  ignorePatterns: ['scripts/**', '*.config.*', '*.cjs', '*.mjs'],
+  ignorePatterns: ['scripts/**', '*.config.*', '*.cjs', '*.mjs', 'recon/**', 'docs/**'],
 };
